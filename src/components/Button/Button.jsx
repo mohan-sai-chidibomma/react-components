59fd066bd24f6f017ee20dbb70dde3798863735c
Mohan Sai <mohansai850@gmail.com>
2025-06-15 15:42:22 +0530

    Button Component and Story Book

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./Button.css";
+import PropTypes from "prop-types";
+
+const Button = ({
+  children,
+  buttonType = "primary",
+  size = "medium",
+  ...props
+}) => {
+  const className = `btn btn-${buttonType} btn-${size}`;
+
+  return (
+    <button className={className} {...props}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  buttonType: PropTypes.oneOf(["primary", "secondary", "danger"]),
+  size: PropTypes.oneOf(["large", "medium", "small"]),
+};
+
+Button.defaultProps = {
+  children: "Button",
+  buttonType: "primary",
+  size: "medium",
+};
+
+export default Button;
