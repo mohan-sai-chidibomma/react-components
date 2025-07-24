ea81f6b8ed4ffd1047a478d9d71e2b2af43595e9
Mohan Sai <mohansai850@gmail.com>
2025-07-24 22:57:42 +0530

    export from index

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { default as Button } from "./src/components/Button/Button";
 export { default as Accordion } from "./src/components/Accordion/Accordion";
+export { default as Carousel } from "./src/components/Carousel/Carousel";
