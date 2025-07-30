ca427f2b340b2e99a33b42707752f7911e1c97a1
Mohan Sai <mohansai850@gmail.com>
2025-07-30 16:33:20 +0530

    export

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export { default as Accordion } from "./src/components/Accordion/Accordion";
 export { default as Carousel } from "./src/components/Carousel/Carousel";
 export { default as Table } from "./src/components/Table/Table";
 export { default as Pagination } from "./src/components/Pagination/Pagination";
+export { default as Card } from "./src/components/Card/Card";
