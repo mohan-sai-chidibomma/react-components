0b8dd162e8b92cfd78c160b7ee87f6c0516e3a81
Mohan Sai <mohansai850@gmail.com>
2025-08-28 21:26:08 +0530

    rm Card comp

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ export { default as Accordion } from "./src/components/Accordion/Accordion";
 export { default as Carousel } from "./src/components/Carousel/Carousel";
 export { default as Table } from "./src/components/Table/Table";
 export { default as Pagination } from "./src/components/Pagination/Pagination";
-export { default as Card } from "./src/components/Card/Card";
 export { default as Testimonial } from "./src/components/Testimonial/Testimonial";
 export { default as Modal } from "./src/components/Modal/Modal";
